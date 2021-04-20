8a23575e3837cee5478d8300c9854e70a14f037a
Mathurin RATIE <mathurin.ratie@epsi.fr>
2021-04-20 18:46:27 +0200

    commentaire cameron joueur

--- joueur.h
+++ joueur.h
@@ -1,23 +1,23 @@
+//Permet la gestion de l'argent dans le tower defense
 class Score {
 private:
-    int score;
-    int niveau;
     int solde;
 public:
     Score();
+    //permet alors plus préscisement le gain d'argent dû à la mort d'un mob
     void gagner_argent(int nb_pieces) {
         this->solde += nb_pieces;
     }
+    //permet alors la vérification du solde disponible
     int get_argent() {
         return this->solde;
     }
+    //permet le paiement lors de l'ajout de tours
     void paiement(int nb_coins) {
         this->solde -= nb_coins;
     }
 };
 
 Score::Score() {
-    this->score = 0;
-    this->niveau = 0;
     this->solde = 200;
 }
